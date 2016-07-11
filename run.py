33b679552d1b1e9fe77018035f3e93abf3b50888
Thomas <thomas.hilfert@ruhr-uni-bochum.de>
2016-07-11 13:53:50 +0200

    Idents and formatting

--- run.py
+++ run.py
@@ -11,18 +11,18 @@ import database
 # stream listener class
 class Streamy(StreamListener):
 	def __init__(self):
-		print("Starting StreamyMcStreamface")
+		print("# Starting StreamyMcStreamface")
 
 	def on_data(self, data):
 		try:
 			jdata = json.loads(data)
 			print(jdata["user"]["name"]+" ["+jdata["created_at"]+"]")
-			print(jdata["text"])
+			print("\t"+jdata["text"])
 			if jdata["place"] is not None:
 				print("\tGeo Supplied: "+jdata["place"]+" ["+jdata["coordinates"]+"]")
 				return True
 		except BaseException as e:
-			print("Error on_data: %s" % str(e))
+			print("# Error on_data: %s" % str(e))
 		return True
 
 	def on_error(self, status):
@@ -42,11 +42,11 @@ api = tweepy.API(auth)
 # access streaming list, if supplied
 list_members = []
 if config["list_name"] == 0:
-	print("Accessing livestream for user ["+api.me().name+"]")
+	print("# Accessing livestream for user ["+api.me().name+"]")
 else:
 	for user in tweepy.Cursor(api.list_members, api.me().name, config["list_name"]).items():
 		list_members.append(user.id_str)
-	print("Accessing livestream for user ["+api.me().name+"] and list ("+config["list_name"]+"), counting "+str(len(list_members))+" list members.")
+	print("# Accessing livestream for user ["+api.me().name+"] and list ("+config["list_name"]+"), counting "+str(len(list_members))+" list members.")
 
 twitter_stream = Stream(auth, Streamy())
 if len(list_members) > 0:
